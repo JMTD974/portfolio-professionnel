804e7df4117cd91995f215f97f8302d85037e6a6
JMTD974 <techerdavid.jm@gmail.com>
2026-05-01 17:27:16 +0400

    feat: ajoute les premieres interactions javascript

---
+++ frontend/src/main.js
@@ -0,0 +1,4 @@
+const title = document.querySelector("#hero-title");
+title.textContent = "Portfolio de David";
+const heroText = document.querySelector(".hero-text");
+heroText.textContent = "Je développe des interfaces web modernes, propres et adaptées aux besoins réels.";
